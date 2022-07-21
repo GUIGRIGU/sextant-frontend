43af9fd6109715206fb6b21b025b984b608fe870
unknown <guilherme.caldeira2002@hotmail.com>
2022-07-21 17:48:29 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src="https://conteudo.imguol.com.br/c/entretenimento/54/2020/04/28/cachorro-pug-1588098472110_v2_1x1.jpg" 
+        className="App-logo" 
+        alt="logo"/>
+        <h1>Something Appropriate</h1>
+        
+          <li class="dropdown">
+            <a className="Exhibit-Component">
+            Exhibit Component</a>
+          <div class="dd-content">
+            <a>Child Component</a>
+            <br></br>
+            <a>Child Component</a>
+            <br></br>
+            <a>Child Component</a>
+          </div>
+          </li>
+         <br></br> 
+         <br></br> 
+         <br></br> 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
